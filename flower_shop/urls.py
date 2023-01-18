c0eecef97d53614d96fbde8ef96243d59eb0056f
KirillYabl <kirill-yablunovskii@mail.ru>
2023-01-18 15:14:38 +0300

    Add urls for flowerapp

--- flower_shop/urls.py
+++ flower_shop/urls.py
@@ -18,17 +18,15 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
-from flowerapp import views
-
-
 urlpatterns = [
-    path('', views.index, name='index'),
     path('admin/', admin.site.urls),
+    path('', include('flowerapp.urls')),
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 if settings.DEBUG:
     import debug_toolbar
+
     urlpatterns = [
-        path(r'__debug__/', include(debug_toolbar.urls)),
-    ] + urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+                      path(r'__debug__/', include(debug_toolbar.urls)),
+                  ] + urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
