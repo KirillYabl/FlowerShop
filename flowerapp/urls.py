2b4dc5cab8132b36459502159e62ab64d30b5969
KirillYabl <kirill-yablunovskii@mail.ru>
2023-01-18 23:40:24 +0300

    Make consultation through standart form

--- flowerapp/urls.py
+++ flowerapp/urls.py
@@ -1,12 +1,7 @@
-from django.urls import path, include
-from rest_framework import routers
+from django.urls import path
 
 from . import views
 
-router = routers.DefaultRouter()
-router.register('consultations', views.ConsultationViewSet)
-
 urlpatterns = [
     path('', views.index, name='index'),
-    path('', include(router.urls)),
 ]
--- flowerapp/views.py
+++ flowerapp/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from rest_framework.viewsets import ModelViewSet
 
+from .forms import ConsultationForm
 from .models import Bouquet
 from .models import Consultation
 from .models import FlowerShop
@@ -10,23 +11,18 @@ from .serializers import ConsultationSerializer
 
 
 def index(request: WSGIRequest) -> HttpResponse:
-    print(request.GET)
     bouquets = Bouquet.objects.filter(is_recommended=True)
     flower_shops = FlowerShop.objects.all()
     context = {
         'bouquets': bouquets,
         'flower_shops': flower_shops,
-        'success_alert_style': request.GET.get('success_alert_style', 'none')
+        'success_alert_style': 'none',
+        'form': ConsultationForm()
     }
+    if request.method == 'POST':
+        context['form'] = ConsultationForm(request.POST)
+        if context['form'].is_valid():
+            context['form'].save()
+            context['success_alert_style'] = 'block'
+
     return render(request, 'index.html', context)
-
-
-class ConsultationViewSet(ModelViewSet):
-    queryset = Consultation.objects.all()
-    serializer_class = ConsultationSerializer
-
-    def create(self, request, *args, **kwargs):
-        super().create(request, *args, **kwargs)
-        response = redirect('index')
-        response['Location'] += '?success_alert_style=block'
-        return response
