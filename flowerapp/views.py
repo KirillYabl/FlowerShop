179a7261e22e3b9c20a6489d2c2663172a468ce8
KirillYabl <kirill-yablunovskii@mail.ru>
2023-01-18 20:49:53 +0300

    Add popup after success record on consultation

--- flowerapp/views.py
+++ flowerapp/views.py
@@ -1,3 +1,5 @@
+from django.core.handlers.wsgi import WSGIRequest
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from rest_framework.viewsets import ModelViewSet
 
@@ -7,10 +9,15 @@ from .models import FlowerShop
 from .serializers import ConsultationSerializer
 
 
-def index(request):
+def index(request: WSGIRequest) -> HttpResponse:
+    print(request.GET)
     bouquets = Bouquet.objects.filter(is_recommended=True)
     flower_shops = FlowerShop.objects.all()
-    context = {'bouquets': bouquets, 'flower_shops': flower_shops}
+    context = {
+        'bouquets': bouquets,
+        'flower_shops': flower_shops,
+        'success_alert_style': request.GET.get('success_alert_style', 'none')
+    }
     return render(request, 'index.html', context)
 
 
@@ -20,4 +27,6 @@ class ConsultationViewSet(ModelViewSet):
 
     def create(self, request, *args, **kwargs):
         super().create(request, *args, **kwargs)
-        return redirect('index')
+        response = redirect('index')
+        response['Location'] += '?success_alert_style=block'
+        return response
