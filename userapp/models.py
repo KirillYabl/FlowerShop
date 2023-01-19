897548025164749fb3d06cea689801f308a7dc4e
KirillYabl <kirill-yablunovskii@mail.ru>
2023-01-19 16:21:23 +0300

    Add roles to users

--- userapp/models.py
+++ userapp/models.py
@@ -1,5 +1,18 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    class Role(models.TextChoices):
+        florist = 'флорист'
+        courier = 'курьер'
+
+    @property
+    def is_florist(self):
+        return self.role == self.Role.florist
+
+    @property
+    def is_courier(self):
+        return self.role == self.Role.courier
+
+    role = models.CharField('роль', max_length=10, choices=Role.choices, null=True, blank=True)
