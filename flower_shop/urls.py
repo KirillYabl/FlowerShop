fbf41f07be15520312ad9f0834eda2ee97b8215f
KirillYabl <kirill-yablunovskii@mail.ru>
2023-01-19 16:53:43 +0300

    Drop media from debug for deploy

--- flower_shop/urls.py
+++ flower_shop/urls.py
@@ -25,10 +25,9 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls'))
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     import debug_toolbar
 
-    urlpatterns = [
-                      path(r'__debug__/', include(debug_toolbar.urls)),
-                  ] + urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += [path(r'__debug__/', include(debug_toolbar.urls)), ]
