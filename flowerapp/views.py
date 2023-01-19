a4af175a2ae42b1a0e5d7b96c0d22618f5231d48
lexa.shvetsoff <lexa.shvetsoff@yandex.ru>
2023-01-19 12:22:52 +0800

    Подключить к фронтенду страницу каталога

--- flowerapp/views.py
+++ flowerapp/views.py
@@ -9,3 +9,13 @@ def index(request):
     flower_shops = FlowerShop.objects.all()
     context = {'bouquets': bouquets, 'flower_shops': flower_shops}
     return render(request, 'index.html', context)
+
+
+def catalog(request):
+    bouquets =Bouquet.objects.all()
+    context = {'bouquets': bouquets}
+    return render(request, 'catalog.html', context)
+
+
+def card(request):
+    return render(request, 'card.html')
