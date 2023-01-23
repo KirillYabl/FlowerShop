c27cd4b76ac7d07dead96e1dcad1214829689503
KirillYabl <kirill-yablunovskii@mail.ru>
2023-01-24 01:36:17 +0300

    Add float support to format_thousands filter

--- flowerapp/templatetags/format_thousands.py
+++ flowerapp/templatetags/format_thousands.py
@@ -9,11 +9,15 @@ register = template.Library()
 def format_thousands(value: str) -> str:
     """Format number by separate thousands: 3600 -> 3 600.
 
-    If number not int it will be int.
     If value not number it will be returned without change.
     """
     try:
-        value = int(value)
+        int_value = int(value)
+        float_value = float(value)
+        if float_value == int_value:
+            value = int_value
+        else:
+            value = float_value
         return f'{value:,}'.replace(',', ' ')
     except ValueError:
         pass
