16682896ca88e9a03938c80b3bd6cff16e20024f
KirillYabl <kirill-yablunovskii@mail.ru>
2023-01-24 20:55:30 +0300

    Add closing backslashes for urls

--- flowerapp/urls.py
+++ flowerapp/urls.py
@@ -5,10 +5,10 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('card/<bouquet_id>/', views.card, name='card'),
-    path('catalog', views.catalog, name='catalog'),
-    path('consultation', views.consultation, name='consultation'),
+    path('catalog/', views.catalog, name='catalog'),
+    path('consultation/', views.consultation, name='consultation'),
     path('order/<bouquet_id>/', views.order, name='order'),
-    path('quiz', views.quiz, name='quiz'),
-    path('result', views.result, name='result'),
-    path('stats', views.stats, name='stats'),
+    path('quiz/', views.quiz, name='quiz'),
+    path('result/', views.result, name='result'),
+    path('stats/', views.stats, name='stats'),
 ]
